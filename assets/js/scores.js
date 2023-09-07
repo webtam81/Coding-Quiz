5cf4426fdb0ec057d5772f963577ef87e0a3a84c
webtam81 <t.h@tammyharris.co.uk>
2023-09-08 00:33:35 +0100

    retrieved scores on scores page and added to list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,27 +1,42 @@
 let clearBtn = document.querySelector('#clear');
 let highscoreEL = document.querySelector('#highscores');
-let scoreArray = JSON.parse(localStorage.getItem('highscores'));
+let scoreArray = [];
 
 clearBtn.addEventListener('click', function() {
     //single score
-    localStorage.removeItem('highscore');
+    //localStorage.removeItem('highscore');
     //list of scores
-    //localStorage.removeItem('highscores');
+    localStorage.removeItem('highscores');
+    scoreArray = [];
     listScores();
 });
 
+function getScores() {
+    if (localStorage.getItem('highscores')) {
+        scoreArray = JSON.parse(localStorage.getItem('highscores'));
+    }
+}
+
 function listScores() {
     //single score
-    if (localStorage.getItem('highscore')) {
-        highscoreEL.innerHTML = '<li>' + localStorage.getItem('highscore'); + '</li>';
-    } else {
-        highscoreEL.innerHTML = "";
-    }
+    //if (localStorage.getItem('highscore')) {
+    //    highscoreEL.innerHTML = '<li>' + localStorage.getItem('highscore'); + '</li>';
+    //} else {
+    //    highscoreEL.innerHTML = "";
+    //}
     //list of scores
     //console.log(scoreArray.length);
     //highscoreEL.innerHTML = '<li>' + localStorage.getItem('highscores'); + '</li>';
+    console.log(scoreArray);
+    console.log(scoreArray.length);
+    if (scoreArray.length > 0) {
+        for (h = 0; h < scoreArray.length; h++) {
+           highscoreEL.innerHTML = highscoreEL.innerHTML +'<li>' + scoreArray[h] + '</li>';
+        }
+    }
 }
 
+getScores();
 listScores(); 
 
 // get high scores from local storage
